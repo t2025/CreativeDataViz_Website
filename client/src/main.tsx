dfc2438267cdc4a944e04648eebd70edc2d86650
Tanvi Ranga <tanviranga@Mac.lan>
2025-08-08 03:44:50 -0400

    Add router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { Router } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <Router hook={useHashLocation}>
+    <App />
+  </Router>
+);
+
